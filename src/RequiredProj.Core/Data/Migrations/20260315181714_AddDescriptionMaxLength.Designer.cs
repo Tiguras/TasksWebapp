dc163009ee6a8c24c09cc89c35561990046ba71f
CameronMilne <cameron.milne@softwarealliance.net>
2026-03-16 18:49:10 +0000

    Add a max length to descrition

--- src/RequiredProj.Core/Data/Migrations/20260315181714_AddDescriptionMaxLength.Designer.cs
+++ src/RequiredProj.Core/Data/Migrations/20260315181714_AddDescriptionMaxLength.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using RequiredProj.Core.Data;
@@ -11,9 +12,11 @@ using RequiredProj.Core.Data;
 namespace RequiredProj.Core.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260315181714_AddDescriptionMaxLength")]
+    partial class AddDescriptionMaxLength
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
